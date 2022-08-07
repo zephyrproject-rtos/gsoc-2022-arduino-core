5c0e43898fb885c6c51952e521d75aa35a2cf4c8
Jonathan Beri <jmberi@gmail.com>
2022-08-07 09:26:46 -0700

    Forgot the "33"

--- variants/variants.h
+++ variants/variants.h
@@ -1,6 +1,6 @@
 #ifdef CONFIG_BOARD_ARDUINO_NANO_33_BLE
-#include "arduino_nano_ble_pinmap.h"
+#include "arduino_nano_33_ble_pinmap.h"
 #endif // CONFIG_BOARD_ARDUINO_NANO_33_BLE
 #ifdef CONFIG_BOARD_ARDUINO_NANO_33_BLE_SENSE
-#include "arduino_nano_ble_sense_pinmap.h"
+#include "arduino_nano_33_ble_sense_pinmap.h"
 #endif // CONFIG_BOARD_ARDUINO_NANO_33_BLE_SENSE
