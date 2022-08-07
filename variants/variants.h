6ecbd9c8ace174d0ded6f0fb22675fb29d6346d6
Jonathan Beri <jmberi@gmail.com>
2022-08-07 10:18:37 -0700

    Add 33 iot to build system

--- variants/variants.h
+++ variants/variants.h
@@ -4,3 +4,6 @@
 #ifdef CONFIG_BOARD_ARDUINO_NANO_33_BLE_SENSE
 #include "arduino_nano_33_ble_sense_pinmap.h"
 #endif // CONFIG_BOARD_ARDUINO_NANO_33_BLE_SENSE
+#ifdef CONFIG_BOARD_ARDUINO_NANO_33_IOT
+#include "arduino_nano_33_iot_pinmap.h"
+#endif // CONFIG_BOARD_ARDUINO_NANO_33_IOT
