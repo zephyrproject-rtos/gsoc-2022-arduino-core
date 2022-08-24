4082ffa420ea8677396eec84c2ffc7a5911ad64f
Mike Szczys <mike@golioth.io>
2022-08-24 14:52:58 -0500

    zephyrSerial: print versus println
    Implement versions with and without linefeed
    Return the char count
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- cores/arduino/zephyrSerial.cpp
+++ cores/arduino/zephyrSerial.cpp
@@ -6,18 +6,30 @@ size_t arduino::ZephyrSerial::begin(unsigned long int baudrate){
     return 0;
 }
 
+size_t arduino::ZephyrSerial::print_char(char ch, bool lf){
+    printk(lf ? "%c\n" : "%c", ch);
+    return lf ? 2 : 1;
+}
+
+size_t arduino::ZephyrSerial::print_str(const char* ptr, bool lf) {
+    printf(lf ? "%s\n" : "%s", ptr);
+    return lf ? strlen(ptr)+1 : strlen(ptr);
+}
+
 size_t arduino::ZephyrSerial::print(char ch){
-    printk("%c",ch);
-    return 0;
+    return print_char(ch, false);
 }
-size_t arduino::ZephyrSerial::println(const char* ptr) {
-    printf("\n%s\n", ptr);
-    return 0;
+
+size_t arduino::ZephyrSerial::println(char ch){
+    return print_char(ch, true);
 }
-size_t arduino::ZephyrSerial::println(char c){
-    // pvt_c = c;
-    printf("\n%c\n",c);
-    return 0;
+
+size_t arduino::ZephyrSerial::print(const char* ptr) {
+    return print_str(ptr, false);
+}
+
+size_t arduino::ZephyrSerial::println(const char* ptr){
+    return print_str(ptr, true);
 }
 
 size_t arduino::ZephyrSerial::println(void){
