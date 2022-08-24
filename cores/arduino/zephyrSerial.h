8e39edc517b1cd77dae369b02927af7c02c09de9
Mike Szczys <mike@golioth.io>
2022-08-24 14:52:58 -0500

    zephyrSerial: print versus println
    Implement versions with and without linefeed
    Return the char count
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- cores/arduino/zephyrSerial.h
+++ cores/arduino/zephyrSerial.h
@@ -7,11 +7,16 @@ namespace arduino {
 
 class ZephyrSerial {
     char pvt_c;
+
+private:
+    size_t print_char(const char c, bool lf);
+    size_t print_str(const char * ptr, bool lf);
+
 public:
     size_t begin(unsigned long int baudrate);  //TODO
 
     size_t print(const char c);
-
+    size_t print(const char * ptr);
     size_t println(const char c);
     size_t println(const char* ptr);
     size_t println(void);
