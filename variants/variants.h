7aa7d7e0b80b97a8d95d962cfbd4f19e55702397
TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
2022-09-20 07:44:21 +0900

    variants: Add common digital pin definition
    The digitalPins enumeration, arduino_pins array,
    and contents of these are determined by d[N]_gpios declaration in dts.
    
    Interlock the declaration with the dts definition.
    
    - Automatically calculate digitalPin enumeration and arduino_pins array
      from devicetree configuration.
    - LED digital pin definition read from 'leds' node in devicetree.
      Append it to enumeration and pins array.
    - static struct gpio_dt_spec d0, d1, ... declaration seem bit redundant.
      Declare as array of gpio_dt_spec.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>

--- variants/variants.h
+++ variants/variants.h
@@ -1,3 +1,7 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#pragma once
+
 #ifdef CONFIG_BOARD_ARDUINO_NANO_33_BLE
 #include <arduino_nano_33_ble_pinmap.h>
 #endif // CONFIG_BOARD_ARDUINO_NANO_33_BLE
@@ -14,3 +18,29 @@
 #include "arduino_mkrzero_pinmap.h"
 #endif // CONFIG_BOARD_ARDUINO_MKRZERO
 
+#define MAX_DIGITAL_PINS 255
+
+#define DN_ENUMS(n, _) COND_CODE_1(DT_NODE_HAS_PROP(DT_PATH(zephyr_user), \
+			d ## n ## _gpios), (D ## n ,), ())
+
+#define LABEL_UPPER_TOKEN_COMMA(nodeid) DT_STRING_UPPER_TOKEN(nodeid, label),
+
+/*
+ * expand as
+ * enum digitalPins { D0, D1, ... LED... NUM_OF_DIGITAL_PINS };
+ */
+enum digitalPins { LISTIFY(MAX_DIGITAL_PINS, DN_ENUMS, ())
+		   DT_FOREACH_CHILD(DT_PATH(leds), LABEL_UPPER_TOKEN_COMMA)
+		   NUM_OF_DIGITAL_PINS };
+
+
+#define NUMBERED_GPIO_DT_SPEC(n, _) \
+	COND_CODE_1(DT_NODE_HAS_PROP(DT_PATH(zephyr_user), d ## n ## _gpios), \
+	      (GPIO_DT_SPEC_GET(DT_PATH(zephyr_user), d ## n ## _gpios),), ())
+
+#define LABELED_GPIO_DT_SPEC(nodeid) GPIO_DT_SPEC_GET_BY_IDX(nodeid, gpios, 0),
+
+static struct gpio_dt_spec arduino_pins[NUM_OF_DIGITAL_PINS] = {
+	LISTIFY(MAX_DIGITAL_PINS, NUMBERED_GPIO_DT_SPEC, ())
+	DT_FOREACH_CHILD(DT_PATH(leds), LABELED_GPIO_DT_SPEC)
+};
