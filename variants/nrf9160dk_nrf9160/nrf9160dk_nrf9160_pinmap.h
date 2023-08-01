976925a6074b2eca7942f9f03742624acd9a274e
dejan <ddeletic@gmail.com>
2023-08-02 03:04:49 +1000

    variants: Add support for nRF9160
    Add support for Nordic nRF9160 DK (Demo Kit) board. More information at https://www.nordicsemi.com/Products/Development-hardware/nRF9160-DK/GetStarte
    
    Signed-off-by: Dejan Deletic <ddeletic@gmail.com>

---
+++ variants/nrf9160dk_nrf9160/nrf9160dk_nrf9160_pinmap.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023 Dejan Deletic
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef INCLUDE_NRF1960DK_NRF1960_PINMAP_H
+#define INCLUDE_NRF1960DK_NRF1960_PINMAP_H
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/kernel.h>
+
+#define NRF9160DK_LED_1         D2          // P0.02
+#define NRF9160DK_LED_2         D3          // P0.03
+#define NRF9160DK_LED_3         D4          // P0.04
+#define NRF9160DK_LED_4         D5          // P0.05
+
+#define NRF9160DK_SWITCH_1      D6          // P0.08
+#define NRF9160DK_SWITCH_2      D7          // P0.09
+#define NRF9160DK_BUTTON_1      D8          // P0.06
+#define NRF9160DK_BUTTON_2      D9          // P0.07
+
+#endif
