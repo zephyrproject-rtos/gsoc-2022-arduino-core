7475287a067efa97942014e99498b10282288a58
Martino Facchin <m.facchin@arduino.cc>
2024-10-01 11:01:50 +0200

    cores: arduino: digitalPins: take into account boards without gpios
    Added a condition that the length of digital_pin_gpios is 0 or more.
    
    -------------------------------------------------------------------
    
    Pick: https://github.com/arduino/ArduinoCore-zephyr/commit/d20ef628
    
    Co-Authored-by:    Martino Facchin <m.facchin@arduino.cc>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -14,6 +14,7 @@
 #include <zephyr/drivers/adc.h>
 #include <zephyr/drivers/i2c.h>
 
+#if DT_PROP_LEN(DT_PATH(zephyr_user), digital_pin_gpios) > 0
 #define DIGITAL_PIN_EXISTS(n, p, i, dev, num)                                                      \
 	(((dev == DT_REG_ADDR(DT_PHANDLE_BY_IDX(n, p, i))) && (num == DT_PHA_BY_IDX(n, p, i, pin))) ?  \
 		 1 :                                                                                       \
@@ -31,6 +32,7 @@
 #endif
 
 #undef DIGITAL_PIN_CHECK_UNIQUE
+#endif
 
 /* Return the index of it if matched, oterwise return 0 */
 #define LED_BUILTIN_INDEX_BY_REG_AND_PINNUM(n, p, i, dev, num)                                     \
@@ -79,7 +81,9 @@
  * enum digitalPins { D0, D1, ... LED... NUM_OF_DIGITAL_PINS };
  */
 enum digitalPins {
+#if DT_PROP_LEN(DT_PATH(zephyr_user), digital_pin_gpios) > 0
 	DT_FOREACH_PROP_ELEM_SEP(DT_PATH(zephyr_user), digital_pin_gpios, DN_ENUMS, (, )),
+#endif
 	NUM_OF_DIGITAL_PINS
 };
 
