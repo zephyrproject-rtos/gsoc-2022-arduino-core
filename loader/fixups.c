82d7eba2930ac49da2c2f331568708069efce974
Martino Facchin <m.facchin@arduino.cc>
2024-10-01 15:57:51 +0200

    nano33ble: add support
    Co-authored-by: Martino Facchin <m.facchin@arduino.cc>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

---
+++ loader/fixups.c
@@ -0,0 +1,36 @@
+#if CONFIG_BOARD_ARDUINO_NANO_33_BLE
+
+#include <cmsis_core.h>
+void disable_mpu_rasr_xn(void)
+{
+	uint32_t index;
+	/* Kept the max index as 8(irrespective of soc) because the sram
+	 * would most likely be set at index 2.
+	 */
+	for (index = 0U; index < 8; index++) {
+		MPU->RNR = index;
+#if defined(CONFIG_ARMV8_M_BASELINE) || defined(CONFIG_ARMV8_M_MAINLINE)
+		if (MPU->RBAR & MPU_RBAR_XN_Msk) {
+			MPU->RBAR ^= MPU_RBAR_XN_Msk;
+		}
+#else
+		if (MPU->RASR & MPU_RASR_XN_Msk) {
+			MPU->RASR ^= MPU_RASR_XN_Msk;
+		}
+#endif /* CONFIG_ARMV8_M_BASELINE || CONFIG_ARMV8_M_MAINLINE */
+	}
+}
+
+int disable_bootloader_mpu() {
+	// MPU was previously enabled in the bootloader
+	// https://github.com/bcmi-labs/zephyr/blob/31cb7dd00fd5bce4c69896b3b2ddf6259d0c0f2b/boards/arm/arduino_nano_33_ble/arduino_nano_33_ble_defconfig#L10C1-L10C15
+	__disable_irq();
+	disable_mpu_rasr_xn();
+	__DMB();
+	MPU->CTRL = 0;
+	__enable_irq();
+    return 0;
+}
+SYS_INIT(disable_bootloader_mpu, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+
+#endif
