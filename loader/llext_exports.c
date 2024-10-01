9e1fbac589f8bbeb384c7362452d08faf60a399e
Martino Facchin <m.facchin@arduino.cc>
2024-10-01 15:34:28 +0200

    Add multitarget loader project
    Co-authored-by: Martino Facchin <m.facchin@arduino.cc>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

---
+++ loader/llext_exports.c
@@ -0,0 +1,121 @@
+#include <string.h>
+#include <zephyr/llext/symbol.h>
+#include <zephyr/usb/usb_device.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define FORCE_EXPORT_SYM(name) \
+       extern void name(void); \
+       EXPORT_SYMBOL(name);
+
+EXPORT_SYMBOL(strrchr);
+EXPORT_SYMBOL(strstr);
+EXPORT_SYMBOL(strncmp);
+EXPORT_SYMBOL(strncpy);
+EXPORT_SYMBOL(strcmp);
+EXPORT_SYMBOL(strlen);
+EXPORT_SYMBOL(strchr);
+EXPORT_SYMBOL(memmove);
+
+EXPORT_SYMBOL(malloc);
+EXPORT_SYMBOL(realloc);
+EXPORT_SYMBOL(calloc);
+EXPORT_SYMBOL(free);
+EXPORT_SYMBOL(rand);
+EXPORT_SYMBOL(srand);
+
+EXPORT_SYMBOL(atof);
+EXPORT_SYMBOL(atol);
+EXPORT_SYMBOL(isspace);
+EXPORT_SYMBOL(isalnum);
+EXPORT_SYMBOL(tolower);
+EXPORT_SYMBOL(toupper);
+
+#if defined(CONFIG_USB_DEVICE_STACK)
+EXPORT_SYMBOL(usb_enable);
+EXPORT_SYMBOL(usb_disable);
+#endif
+
+EXPORT_SYMBOL(z_log_msg_runtime_vcreate);
+
+#if defined(CONFIG_NETWORKING)
+FORCE_EXPORT_SYM(net_if_foreach);
+FORCE_EXPORT_SYM(net_if_get_by_iface);
+#endif
+
+#if defined(CONFIG_NET_DHCPV4)
+FORCE_EXPORT_SYM(net_dhcpv4_start);
+#if defined(CONFIG_NET_DHCPV4_OPTION_CALLBACKS)
+FORCE_EXPORT_SYM(net_dhcpv4_add_option_callback);
+#endif
+#endif
+
+#if defined(CONFIG_NET_MGMT_EVENT)
+FORCE_EXPORT_SYM(net_mgmt_add_event_callback);
+#endif
+
+
+#if defined(CONFIG_NET_SOCKETS)
+FORCE_EXPORT_SYM(getaddrinfo);
+FORCE_EXPORT_SYM(socket);
+FORCE_EXPORT_SYM(connect);
+FORCE_EXPORT_SYM(send);
+FORCE_EXPORT_SYM(recv);
+FORCE_EXPORT_SYM(open);
+FORCE_EXPORT_SYM(close);
+EXPORT_SYMBOL(exit);
+#endif
+
+#if defined(CONFIG_CDC_ACM_DTE_RATE_CALLBACK_SUPPORT)
+FORCE_EXPORT_SYM(cdc_acm_dte_rate_callback_set);
+#endif
+
+FORCE_EXPORT_SYM(k_timer_init);
+//FORCE_EXPORT_SYM(k_timer_user_data_set);
+//FORCE_EXPORT_SYM(k_timer_start);
+
+EXPORT_SYMBOL(sin);
+EXPORT_SYMBOL(cos);
+EXPORT_SYMBOL(tan);
+EXPORT_SYMBOL(atan);
+
+EXPORT_SYMBOL(puts);
+EXPORT_SYMBOL(putchar);
+EXPORT_SYMBOL(printf);
+EXPORT_SYMBOL(sprintf);
+EXPORT_SYMBOL(snprintf);
+EXPORT_SYMBOL(cbvprintf);
+
+FORCE_EXPORT_SYM(abort);
+#if defined(CONFIG_RING_BUFFER)
+FORCE_EXPORT_SYM(ring_buf_get);
+FORCE_EXPORT_SYM(ring_buf_peek);
+FORCE_EXPORT_SYM(ring_buf_put);
+#endif
+EXPORT_SYMBOL(sys_clock_cycle_get_32);
+FORCE_EXPORT_SYM(__aeabi_dcmpun);
+FORCE_EXPORT_SYM(__aeabi_dcmple);
+FORCE_EXPORT_SYM(__aeabi_d2lz);
+FORCE_EXPORT_SYM(__aeabi_uldivmod);
+FORCE_EXPORT_SYM(__aeabi_ui2d);
+FORCE_EXPORT_SYM(__aeabi_dcmplt);
+FORCE_EXPORT_SYM(__aeabi_ddiv);
+FORCE_EXPORT_SYM(__aeabi_dmul);
+FORCE_EXPORT_SYM(__aeabi_d2f);
+FORCE_EXPORT_SYM(__aeabi_fcmpun);
+FORCE_EXPORT_SYM(__aeabi_dadd);
+FORCE_EXPORT_SYM(__aeabi_fcmple);
+FORCE_EXPORT_SYM(__aeabi_idiv);
+FORCE_EXPORT_SYM(__aeabi_dcmpgt);
+FORCE_EXPORT_SYM(__aeabi_dsub);
+FORCE_EXPORT_SYM(__aeabi_i2d);
+FORCE_EXPORT_SYM(__aeabi_uidiv);
+FORCE_EXPORT_SYM(__aeabi_l2d);
+FORCE_EXPORT_SYM(__aeabi_d2uiz);
+FORCE_EXPORT_SYM(__aeabi_uidivmod);
+FORCE_EXPORT_SYM(__aeabi_dcmpeq);
+FORCE_EXPORT_SYM(__aeabi_d2iz);
+FORCE_EXPORT_SYM(__aeabi_f2d);
+FORCE_EXPORT_SYM(__aeabi_idivmod);
---
+++ loader/main.c
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2024 Arduino SA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(app);
+
+#include <zephyr/kernel.h>
+#include <zephyr/storage/flash_map.h>
+#include <zephyr/llext/llext.h>
+#include <zephyr/llext/buf_loader.h>
+#include <zephyr/shell/shell.h>
+
+#include <stdlib.h>
+
+static int loader(const struct shell *sh)
+{
+	const struct flash_area *fa;
+	int rc;
+
+	/* Test that attempting to open a disabled flash area fails */
+	rc = flash_area_open(FIXED_PARTITION_ID(user_sketch), &fa);
+	if (rc) {
+		printk("Failed to open flash area, rc %d\n", rc);
+		return rc;
+	}
+
+	char header[16];
+	rc = flash_area_read(fa, 0, header, sizeof(header));
+	if (rc) {
+		printk("Failed to read header, rc %d\n", rc);
+		return rc;
+	}
+
+	if (!header[0] || header[0] == 0xff) {
+		printk("No sketch found\n");
+		return -ENOENT;
+	}
+
+	char *endptr;
+	size_t sketch_buf_len = strtoul(header, &endptr, 10);
+	// make sure number got parsed correctly"
+	if (header == endptr) {
+		printk("Failed to parse sketch size\n");
+		return -EINVAL;
+	}
+
+	uint8_t debug = endptr[1];
+	if (debug != 0 && strcmp(k_thread_name_get(k_current_get()), "main") == 0) {
+		// starts the shell
+		return 0;
+	}
+
+	int header_len = endptr - header + 2;
+
+	uint8_t* sketch_buf = k_aligned_alloc(4096, sketch_buf_len);
+
+	rc = flash_area_read(fa, header_len, sketch_buf, sketch_buf_len);
+	if (rc) {
+		printk("Failed to read sketch area, rc %d\n", rc);
+		return rc;
+	}
+
+	struct llext_buf_loader buf_loader = LLEXT_BUF_LOADER(sketch_buf, sketch_buf_len);
+	struct llext_loader *ldr = &buf_loader.loader;
+
+	LOG_HEXDUMP_DBG(sketch_buf, 4, "4 byte MAGIC");
+
+	struct llext_load_param ldr_parm = LLEXT_LOAD_PARAM_DEFAULT;
+	struct llext *ext;
+	int res;
+
+	res = llext_load(ldr, "sketch", &ext, &ldr_parm);
+	if (res) {
+		printk("Failed to load sketch, rc %d\n", res);
+		return res;
+	}
+
+	void (*main_fn)() = llext_find_sym(&ext->exp_tab, "main");
+	if (!main_fn) {
+		printk("Failed to find main function\n");
+		return -ENOENT;
+	}
+
+	llext_bootstrap(ext, main_fn, NULL);
+
+	return 0;
+}
+
+#if CONFIG_SHELL
+SHELL_CMD_REGISTER(sketch, NULL, "Run sketch", loader);
+#endif
+
+int main(void)
+{
+	loader(NULL);
+	return 0;
+}
+
