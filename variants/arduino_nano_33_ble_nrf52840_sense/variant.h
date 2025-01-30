17311e35164a090d1d5e5973c4a788d47f5a0aa4
Mike S <CyberMerln@gmail.com>
2025-01-30 15:57:46 -0500

    variants: Added legacy pin coverager for nano 33 boards
    Co-authored-by: Mike S <CyberMerln@gmail.com>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- variants/arduino_nano_33_ble_nrf52840_sense/variant.h
+++ variants/arduino_nano_33_ble_nrf52840_sense/variant.h
@@ -3,3 +3,11 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+// TODO: correctly handle these legacy defines
+#define MOSI    11
+#define MISO    12
+#define SCK     13
+#define SS      10
+#define SDA     0
+#define SCL     0
