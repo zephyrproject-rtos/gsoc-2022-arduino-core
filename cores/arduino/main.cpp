4f283364d9d8a6a0fd0ab9b862cb7435fa27e301
Martino Facchin <m.facchin@arduino.cc>
2025-02-12 13:13:34 +0100

    wip: linked build
    Note by committer: Only under the `cores/arduino` was picked.
    
    Co-authored-by: Martino Facchin <m.facchin@arduino.cc>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- cores/arduino/main.cpp
+++ cores/arduino/main.cpp
@@ -5,6 +5,8 @@
  */
 
 #include "Arduino.h"
+#include "zephyr/kernel.h"
+#include <cstdint>
 #ifdef CONFIG_LLEXT
 #include <zephyr/llext/symbol.h>
 #endif
@@ -46,3 +48,47 @@ int main(void) {
 #ifdef CONFIG_LLEXT
 LL_EXTENSION_SYMBOL(main);
 #endif
+
+/* These magic symbols are provided by the linker.  */
+extern void (*__preinit_array_start []) (void) __attribute__((weak));
+extern void (*__preinit_array_end []) (void) __attribute__((weak));
+extern void (*__init_array_start []) (void) __attribute__((weak));
+extern void (*__init_array_end []) (void) __attribute__((weak));
+
+static void __libc_init_array (void)
+{
+  size_t count;
+  size_t i;
+
+  count = __preinit_array_end - __preinit_array_start;
+  for (i = 0; i < count; i++)
+    __preinit_array_start[i] ();
+
+  count = __init_array_end - __init_array_start;
+  for (i = 0; i < count; i++)
+    __init_array_start[i] ();
+}
+
+extern "C" __attribute__((section(".entry_point"), used)) void entry_point(struct k_heap* stack, size_t stack_size) {
+  // copy .data in the right place
+  // .bss should already be in the right place
+  // call constructors
+  extern uintptr_t _sidata;
+  extern uintptr_t _sdata;
+  extern uintptr_t _edata;
+  extern uintptr_t _sbss;
+  extern uintptr_t _ebss;
+  extern uintptr_t __heap_start;
+  extern uintptr_t __heap_end;
+  //__asm volatile ("cpsie i");
+
+  const size_t alignment = 4096;
+  printk("Heap end: %p\n", &__heap_end);
+  printk("Heap start: %p\n", &__heap_start);
+  // __heap_start = (__heap_start + (alignment - 1)) & ~(alignment - 1);
+
+  memcpy(&_sdata, &_sidata, &_edata - &_sdata);
+  memset(&_sbss, 0, &_ebss - &_sbss);
+  __libc_init_array();
+  main();
+}
