ba941ee5e2dbf23763945b8766717b114e3971e1
Kurt Eckhardt <kurte@rockisland.com>
2025-03-05 11:28:36 -0800

    Add call to initVariant in main move ble sense to variant.cpp
    Added call to main.cpp to initVariant.
    Added a weak version in main.cpp.
    
    Then moved the code for initializing the enable sensor pin out of main.cpp
    into the initVariant that I added to variant.cpp for the specific board
    
    Note by committer: Only under the `cores/arduino` was picked.
    
    Co-authored-by: Kurt Eckhardt <kurte@rockisland.com>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- cores/arduino/main.cpp
+++ cores/arduino/main.cpp
@@ -13,33 +13,24 @@
 void start_static_threads();
 #endif
 
-#ifdef CONFIG_GPIO_NRFX
-#include <zephyr/dt-bindings/gpio/nordic-nrf-gpio.h>
-#endif
+
+// This function will be overwriten by most variants.
+void __attribute__((weak))initVariant(void) {
+
+}
+
 
 int main(void) {
 #if (DT_NODE_HAS_PROP(DT_PATH(zephyr_user), cdc_acm) && CONFIG_USB_CDC_ACM)
   Serial.begin(115200);
 #endif
 
+  initVariant();
+
 #ifdef CONFIG_MULTITHREADING
   start_static_threads();
 #endif
 
-#ifdef CONFIG_GPIO_NRFX
-  static const struct gpio_dt_spec enable_sensors =
-    GPIO_DT_SPEC_GET(DT_PATH(zephyr_user), pin_enable_gpios);
-  if (gpio_is_ready_dt(&enable_sensors)) {
-      gpio_flags_t flags = enable_sensors.dt_flags | GPIO_OUTPUT_HIGH | GPIO_ACTIVE_HIGH | NRF_GPIO_DRIVE_H0H1;
-      Serial.println((uint32_t)flags, HEX);
-      
-      gpio_pin_configure(enable_sensors.port, enable_sensors.pin, flags);
-      gpio_pin_set(enable_sensors.port, enable_sensors.pin, HIGH);
-      //Serial.println("### Sensor pin enabled ###");
-
-      delay(500);
-  }
-#endif
   setup();
 
   for (;;) {
