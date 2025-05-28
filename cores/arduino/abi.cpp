1474d8a567cf6939a53bbffb03b73c60c75954d9
Martino Facchin <m.facchin@arduino.cc>
2025-05-28 15:51:14 +0200

    loader: impl: add some libc/::std functions
    Note by committer: Only under the `cores/arduino` was picked.
    
    Co-authored-by: Martino Facchin <m.facchin@arduino.cc>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- cores/arduino/abi.cpp
+++ cores/arduino/abi.cpp
@@ -12,9 +12,15 @@ extern "C" int __cxa_atexit(void (*func) (void *), void * arg, void * dso_handle
   (void)func; (void)arg; (void)dso_handle; // unused
   return 0;
 }
+extern "C" int atexit(void (*func)(void)) {
+  (void)func;
+  return 0;
+}
 
 namespace std {
   void __throw_length_error(const char* __s __attribute__((unused))) {}
+  void __throw_bad_alloc() {}
+  void __throw_bad_function_call() {}
 };
 
 extern "C" int strcmp(const char* s1, const char* s2) {
