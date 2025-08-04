63e52b889e1cd1040f1b00d561622fa5dd3d5238
Luca Burelli <l.burelli@arduino.cc>
2025-08-04 14:27:01 +0200

    cores/arduino/abi: squash warning
    Use a global 'extern "C" { }' block to avoid warnings about variables
    being initialized and declared extern.
    
    Signed-off-by: Luca Burelli <l.burelli@arduino.cc>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- cores/arduino/abi.cpp
+++ cores/arduino/abi.cpp
@@ -6,24 +6,29 @@
 
 #include <stdlib.h>
 
-extern "C" void __cxa_pure_virtual(void) {}
-extern "C" void __cxa_deleted_virtual(void) {}
-extern "C" int __cxa_atexit(void (*func) (void *), void * arg, void * dso_handle) {
-  (void)func; (void)arg; (void)dso_handle; // unused
-  return 0;
-}
-extern "C" int atexit(void (*func)(void)) {
-  (void)func;
-  return 0;
-}
-
 namespace std {
   void __throw_length_error(const char* __s __attribute__((unused))) {}
   void __throw_bad_alloc() {}
   void __throw_bad_function_call() {}
 };
 
-extern "C" int strcmp(const char* s1, const char* s2) {
+extern "C" {
+
+void* __dso_handle = (void*) &__dso_handle;
+
+void __cxa_pure_virtual(void) {}
+void __cxa_deleted_virtual(void) {}
+int __cxa_atexit(void (*func) (void *), void * arg, void * dso_handle) {
+  (void)func; (void)arg; (void)dso_handle; // unused
+  return 0;
+}
+
+int atexit(void (*func)(void)) {
+  (void)func;
+  return 0;
+}
+
+int strcmp(const char* s1, const char* s2) {
   while(*s1 && (*s1 == *s2))
   {
     s1++;
@@ -31,3 +36,5 @@ extern "C" int strcmp(const char* s1, const char* s2) {
   }
   return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
+
+} /* extern "C" */
