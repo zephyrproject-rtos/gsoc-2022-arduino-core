8c85e3616b75429b240eda7431e52653a15a9621
Sebastian Romero <s.romero@arduino.cc>
2025-09-17 18:36:44 +0200

    cores: arduino: add macro to find pins by node
    Add `DIGITAL_PIN_GPIOS_FIND_NODE` helper macro.
    
    -------------------------------------------------------------------
    
    Pick: https://github.com/arduino/ArduinoCore-zephyr/commit/a60cd82a
    
    Co-Authored-by: Sebastian Romero <s.romero@arduino.cc>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -35,6 +35,11 @@
 #undef DIGITAL_PIN_CHECK_UNIQUE
 #endif
 
+// Helper macro to get Arduino pin number from device tree alias
+#define DIGITAL_PIN_GPIOS_FIND_NODE(node)                                                          \
+	DIGITAL_PIN_GPIOS_FIND_PIN(DT_REG_ADDR(DT_PHANDLE_BY_IDX(node, gpios, 0)),                     \
+							   DT_PHA_BY_IDX(node, gpios, 0, pin))
+
 /* Return the index of it if matched, oterwise return 0 */
 #define LED_BUILTIN_INDEX_BY_REG_AND_PINNUM(n, p, i, dev, num)                                     \
 	(DIGITAL_PIN_EXISTS(n, p, i, dev, num) ? i : 0)
