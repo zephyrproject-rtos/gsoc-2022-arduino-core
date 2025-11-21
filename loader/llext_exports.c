334c3503079bbcd37c210b1c5146761d7f2112d9
Mattia Pennasilico <m.pennasilico@arduino.cc>
2025-11-21 15:14:49 +0100

    llext: add vsnprintf to exported symbols
    vsnprintf is needed to compile Arduino_ConnectionHandler library
    
    Co-authored-by: Mattia Pennasilico <m.pennasilico@arduino.cc>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- loader/llext_exports.c
+++ loader/llext_exports.c
@@ -233,6 +233,7 @@ EXPORT_SYMBOL(printf);
 EXPORT_SYMBOL(sprintf);
 EXPORT_SYMBOL(snprintf);
 EXPORT_SYMBOL(cbvprintf);
+EXPORT_SYMBOL(vsnprintf);
 FORCE_EXPORT_SYM(abort);
 EXPORT_SYMBOL(sscanf);
 
