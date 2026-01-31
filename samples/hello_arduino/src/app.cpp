24b2346746066205cf423031aa96edf7d4dceb3d
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-01-31 22:11:23 +0900

    samples: hello_arduino: Correct printk format specifier
    Fixed to use %zd which corresponds to size_t.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- samples/hello_arduino/src/app.cpp
+++ samples/hello_arduino/src/app.cpp
@@ -16,14 +16,14 @@ void loop() {
   size_t ret2;
   ret1 = Serial.print(c);
   ret2 = Serial.println("Hello, World!");
-  printk("Sizes: %d %d\n", ret1, ret2);
+  printk("Sizes: %zd %zd\n", ret1, ret2);
   Serial.println();
   ret1 = Serial.print("My letter is: ");
   ret2 = Serial.println(c);
-  printk("Sizes: %d %d\n", ret1, ret2);
+  printk("Sizes: %zd %zd\n", ret1, ret2);
   Serial.println();
   char myString[] = "Will it print?";
   ret1 = Serial.println(myString);
-  printk("Size: %d \n\n\n", ret1);
+  printk("Size: %zd \n\n\n", ret1);
   delay(1000); // 1 second delay
 }
