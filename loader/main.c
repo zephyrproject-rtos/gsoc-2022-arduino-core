pennam/ArduinoCore-zephyr

UNO Q: initial import

loader/main.c
@@ -90,6 +90,11 @@ static int loader(const struct shell *sh)
 		return rc;
 	}
 
+	#if defined(CONFIG_BOARD_B_U585I_IOT02A)
+	void matrixBegin(void);
+	matrixBegin();
+	#endif
+
 	struct sketch_header_v1 *sketch_hdr = (struct sketch_header_v1 *)(header + 7);
 	if (sketch_hdr->ver != 0x1 || sketch_hdr->magic != 0x2341) {
 		printk("Invalid sketch header\n");

zephyrproject-rtos/gsoc-2022-arduino-core

Co-authored-by: Martino Facchin <m.facchin@arduino.cc>
Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

loader/main.c
@@ -108,6 +108,11 @@ static int loader(const struct shell *sh) {
 		return rc;
 	}
 
+#if defined(CONFIG_BOARD_ARDUINO_UNO_Q)
+	void matrixBegin(void);
+	matrixBegin();
+#endif
+
 	struct sketch_header_v1 *sketch_hdr = (struct sketch_header_v1 *)(header + 7);
 	if (sketch_hdr->ver != 0x1 || sketch_hdr->magic != 0x2341) {
 		printk("Invalid sketch header\n");