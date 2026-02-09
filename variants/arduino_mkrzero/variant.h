4f0c586820fd30f926ba6112334d31fc40efb25f
Dhruva Gole <d-gole@ti.com>
2026-02-09 18:39:17 +0530

    variants: remove empty variant.h
    There was an unnecessary dependency on having empty variant.h
    files in each variant. Remove this if it's unused and use a
    default empty one instead. This means we shall only require an
    overlay when adding new platform support here.
    
    update the documentation with this as well
    
    Signed-off-by: Dhruva Gole <d-gole@ti.com>

--- variants/arduino_mkrzero/variant.h
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright (c) 2022 Dhruva Gole
- *
- * SPDX-License-Identifier: Apache-2.0
- */
--- variants/pocketbeagle_2_am6254_a53/variant.h
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2025 Ayush Singh <ayush@beagleboard.org>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#pragma once
