145445832a8c1c2fd7c5c368ecbaf4ae70d6e921
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-04-18 08:27:28 +0900

    cores: arduino: Extract definitions used for compile-time calculations
    Move constexpr-specified constants and functions used for compile-time
    calculations from zephyrCommon.cpp to wiring_private.h.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

---
+++ cores/arduino/wiring_private.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) Arduino s.r.l. and/or its affiliated companies
+ * Copyright (c) 2026 KurtE
+ * Copyright (c) 2026 TOKITA Hiroshi
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+
+namespace zephyr {
+namespace arduino {
+
+constexpr struct gpio_dt_spec arduino_pins[] = {
+	DT_FOREACH_PROP_ELEM_SEP(
+	DT_PATH(zephyr_user), digital_pin_gpios, GPIO_DT_SPEC_GET_BY_IDX, (, ))};
+
+/*
+ * Calculate GPIO ports/pins number statically from devicetree configuration
+ */
+
+template <class N, class Head> constexpr N sum_of_list(const N sum, const Head &head) {
+	return sum + head;
+}
+
+template <class N, class Head, class... Tail>
+constexpr N sum_of_list(const N sum, const Head &head, const Tail &...tail) {
+	return sum_of_list(sum + head, tail...);
+}
+
+template <class N, class Head> constexpr N max_in_list(const N max, const Head &head) {
+	return (max >= head) ? max : head;
+}
+
+template <class N, class Head, class... Tail>
+constexpr N max_in_list(const N max, const Head &head, const Tail &...tail) {
+	return max_in_list((max >= head) ? max : head, tail...);
+}
+
+template <class Query, class Head>
+constexpr size_t is_first_appearance(const size_t &idx, const size_t &at, const size_t &found,
+									 const Query &query, const Head &head) {
+	return ((found == ((size_t)-1)) && (query == head) && (idx == at)) ? 1 : 0;
+}
+
+template <class Query, class Head, class... Tail>
+constexpr size_t is_first_appearance(const size_t &idx, const size_t &at, const size_t &found,
+									 const Query &query, const Head &head, const Tail &...tail) {
+	return ((found == ((size_t)-1)) && (query == head) && (idx == at)) ?
+			   1 :
+			   is_first_appearance(idx + 1, at, (query == head ? idx : found), query, tail...);
+}
+
+#define ZARD_GET_DEVICE_VARGS(n, p, i, _) DEVICE_DT_GET(DT_GPIO_CTLR_BY_IDX(n, p, i))
+#define ZARD_FIRST_APPEARANCE(n, p, i)                                                             \
+	is_first_appearance(0, i, ((size_t)-1), DEVICE_DT_GET(DT_GPIO_CTLR_BY_IDX(n, p, i)),           \
+						DT_FOREACH_PROP_ELEM_SEP_VARGS(n, p, ZARD_GET_DEVICE_VARGS, (, ), 0))
+
+#if DT_PROP_LEN(DT_PATH(zephyr_user), digital_pin_gpios) > 0
+
+constexpr int port_num = sum_of_list(
+	0, DT_FOREACH_PROP_ELEM_SEP(DT_PATH(zephyr_user), digital_pin_gpios,
+            ZARD_FIRST_APPEARANCE, (, )));
+
+#define ZARD_GPIO_NGPIOS(n, p, i) DT_PROP(DT_GPIO_CTLR_BY_IDX(n, p, i), ngpios)
+constexpr int max_ngpios = max_in_list(
+	0, DT_FOREACH_PROP_ELEM_SEP(DT_PATH(zephyr_user), digital_pin_gpios, ZARD_GPIO_NGPIOS, (, )));
+
+#else
+
+constexpr int port_num = 1;
+constexpr int max_ngpios = 0;
+
+#endif
+
+} // namespace arduino
+} // namespace zephyr
+
+#endif // __cplusplus
