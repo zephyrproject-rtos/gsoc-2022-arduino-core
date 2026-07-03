e0325f1d78b760e1919fe9d1bae1fd7116e0977a
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-07-04 08:59:24 +0900

    variants: Add support for Arduino Nano Matter
    Add configuration files for Arduino Nano Matter.
    
    We have confirmed that it compiles when used in combination with
    https://github.com/arduino/zephyr/tree/zephyr-arduino-v4.2.0.
    
    Co-authored-by: Daniele Cloralio <d.cloralio@arduino.cc>
    Co-authored-by: Luca Burelli <l.burelli@arduino.cc>
    Co-authored-by: Tamas Jozsi <tamas.jozsi@silabs.com>
    Co-authored-by: pennam <m.pennasilico@arduino.cc>
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

---
+++ variants/arduino_nano_matter_mgm240sd22vna/variant.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Arduino s.r.l. and/or its affiliated companies
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#define MOSI    11
+#define MISO    12
+#define SCK     13
+#define SS      10
+#define SDA     4
+#define SCL     5
+
+#define LED_BUILTIN_1 23
+#define LED_BUILTIN_2 24
+#define LED_BUILTIN_ACTIVE (LOW)
+#define LED_BUILTIN_INACTIVE (!LED_BUILTIN_ACTIVE)
+
+#define BTN_BUILTIN 25
